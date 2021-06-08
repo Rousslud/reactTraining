5972e34dc9fda9ea478e55ab1ae64136de1e86ec
ROUSSELOT Ludovic <rousselot.ludovic@yahoo.fr>
2021-06-08 14:13:29 +0200

    Etat initial du projet

---
+++ src/App/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Bonjour les amis</h1>
+      <p> Ceci est une application en React</p>
+      <h2>Un autre exemple de titre</h2>
+      <p>Avec un autre sous paragraphe</p>
+    </div>
+  );
+}
+
+export default App;
