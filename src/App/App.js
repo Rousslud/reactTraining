0b9062c455947e8eb1160f5f08a7a7cefac1552c
ROUSSELOT Ludovic <rousselot.ludovic@yahoo.fr>
2021-06-09 10:04:50 +0200

    Changement conditionnel maChaine

--- src/App/App.js
+++ src/App/App.js
@@ -6,7 +6,7 @@ import Button from './components/Button/Button';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { counter: 1, machaine: 'Hello' };
+    this.state = { counter: 0, machaine: 'Hello' };
   }
   componentDidUpdate() {
     console.log(arguments);
@@ -16,7 +16,7 @@ class App extends React.Component {
     return <div className="App">
       Notre compteur : {this.state.counter} {this.state.machaine};
       <Button bgColor="green" clickActionPerso={argument => {
-        this.setState({ counter: this.state.counter + 1 })
+        this.setState({ counter: this.state.counter + 1, machaine:(this.state.counter+1===2?'On a atteint 2':'Hello') })
         console.log("Depuis app : " + this.state.counter);
       }}>
         <img src="https://cdn3.iconfinder.com/data/icons/materia-flat-halloween-free/24/039_026_cat_black_witch_halloween-256.png" alt="click" />Miaoouuu</Button>
