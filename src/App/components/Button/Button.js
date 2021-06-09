4b28f4b2080b89378c56c739164fdbf9069a11a5
ROUSSELOT Ludovic <rousselot.ludovic@yahoo.fr>
2021-06-09 10:22:08 +0200

    Changement couleur sur clic bouton

--- src/App/components/Button/Button.js
+++ src/App/components/Button/Button.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Button.css';
 import PropTypes from "prop-types";
 
 function Button(props) {
 	console.log(props)
+	const[clicked, setClicked] = useState(false);
 	return <button onClick={evt => {
 		//alert('j\'ai cliqué sur ' + evt.target.innerText)
 		console.warn(evt);
 		props.clickActionPerso(evt.target.innerText);
+		setClicked(true);
+		// Remise à false de l'état cliqué du bouton au bout de 200ms
+		setTimeout(()=>{setClicked(false)}, 200)
 	}}
-		className="Button"
+		className={"Button"+(clicked? ' clicked':'')}
 		style={{ color: props.fontColor, backgroundColor: props.bgColor, fontStyle: (props.isItalic ? "italic" : "normal") }}>
 		{props.title || props.children}</button>
 }
